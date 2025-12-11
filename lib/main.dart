9ec67b40645162e1fd21f5e761aaa2b8e8de7e13
mawada <alhijrymawada@gmail.com>
2025-12-11 12:27:11 +0300

    added new code in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,31 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyInteractiveApp());
+  runApp(MyStatelessApp());
 }
 
-class MyInteractiveApp extends StatefulWidget {
-  @override
-  _MyInteractiveAppState createState() => _MyInteractiveAppState();
-}
-
-class _MyInteractiveAppState extends State<MyInteractiveApp> {
-  String message = 'Welcome to Flutter!';
-  int counter = 0;
-
-  void updateMessage() {
-    setState(() {
-      message = 'Button Pressed!';
-      counter++;
-    });
-  }
-
+class MyStatelessApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Interactive UI'),
+          title: Text('Stateless Widget Example'),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(message),
-              Text("$counter"),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: updateMessage,
-                child: Text('Press Me'),
-              ),
-            ],
-          ),
+          child: MyCustomWidget(),
         ),
       ),
+    );
+  }
+}
+
+class MyCustomWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      'Hello, I am a Stateless Widget!',
+      style: TextStyle(fontSize: 24),
     );
   }
 }
