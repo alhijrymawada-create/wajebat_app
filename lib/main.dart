8b7736fed76a4f3f5060b9b64e85b023370fce02
mawada <alhijrymawada@gmail.com>
2025-12-11 19:01:48 +0300

    added new code in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
 import 'dart:io';
+
+import 'textfiles.dart';
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/foundation.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'sqldb.dart';
+
 import 'package:path_provider/path_provider.dart';
+String x="";
 
 void main() {
   runApp(const MyApp());
@@ -11,11 +17,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -26,6 +34,8 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
+
+
   final String title;
 
   @override
@@ -33,126 +43,59 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  SqlDb sqlDb = SqlDb();
+
   int _counter = 0;
+  Textfiles fileobj = Textfiles();
 
+  late String initialfilecontaint="";
+  late Future<String> filecontaint;
 
-  void _readTextFile() async{
-    final Directory tempDir = await getTemporaryDirectory();
-    print(tempDir);
-
-    final File file = File('${tempDir.path}/file_picker/a text file 005.txt');
-
-    final String fileContent = await file.readAsString();
-
-    print(fileContent);
-
-    setState(() {
-
-      filecontaint=fileContent.toString();
-
-    });
-  }
-  void _readTextFile2(File? path) async{
-    final Directory tempDir = await getTemporaryDirectory();
-    print(tempDir);
-
-    //final File file = File(path);
-
-    final String fileContent = await path!.readAsString();
-
-    print(fileContent);
-
-    setState(() {
-
-      filecontaint=fileContent.toString();
-
-    });
-  }
-  void _saveTextFile() async{
-    // this next 4 line to write a file in temp Directory
-    final Directory tempDir = await getTemporaryDirectory();
-    final File file = File('/storage/emulated/0/Download/'+filename.text+".text");
-    print(""+tempDir.toString());
-    await file.writeAsString(newnote.text);
-    setState(() {
-      newnote.text="";
-      filename.text="";
-
-
-    });
-  }
-
-  void _incrementCounter() async{
-
-
-
-    /*////////////here we creat a file in side our own Dir in temp folder
-    final Directory tempDir = await getTemporaryDirectory();
-    final Directory newDirectory =
-    Directory('${tempDir.path}/sample_directoryNo'+_counter.toString());
-
-// Always check that the directory exists
-    if (await newDirectory.exists() == false) {
-      await newDirectory.create();
-    }
-
-    final File file = File('${newDirectory.path}/sample_file'+_counter.toString()+'.txt');
-    print(""+tempDir.toString());
-    await file.writeAsString('this file writen as the try No.'+_counter.toString());*/
-
-    //////////////// reading a file from temp
-
-    // final appDocuments = await getApplicationDocumentsDirectory();
-    //print(appDocuments);
-    //print("mazin");
-    setState(() {
-
-      _counter++;
-    });
-  }
-  late String filecontaint="";
   TextEditingController newnote=TextEditingController();
   TextEditingController filename=TextEditingController();
+  TextEditingController dirName=TextEditingController();
 
 
   @override
   Widget build(BuildContext context) {
     File? file;
     FilePickerResult? result;
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
+
         title: Text(widget.title),
       ),
       body: Center(
         child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Text(filecontaint),
+          Text(x),
           ElevatedButton(
             onPressed: () async {
+              // x=filecontaint as String;
+
               try {
                 result = await FilePicker.platform.pickFiles();
                 if (result != null) {
                   if (!kIsWeb) {
                     file = File(result!.files.single.path!);
-                    _readTextFile2(file);
+                    filecontaint = (await fileobj.readStudentslist(file)) as Future<String>;
+                    //initialfilecontaint=filecontaint;
+                    // x='${filecontaint}'.toString();
+
+
                   }
-                  setState(() {});
+
                 } else {
                   print("user cancled the picker");
                   // User canceled the picker
                 }
+
               } catch (_) {}
+              super.setState(()  {
+
+              });
             },
             child: const Text('Pick a Text file File'),
           ),
@@ -164,9 +107,10 @@ class _MyHomePageState extends State<MyHomePage> {
               labelText: 'file Name',
               hintText: 'Enter the a file to name to save the note to ',
             ),
-          ),TextField(
+          ),
+          TextField(
             controller: newnote,
-            maxLines: 4,
+            maxLines: 2,
             decoration: InputDecoration(
               border: OutlineInputBorder(),
               labelText: 'Enter a note',
@@ -175,16 +119,65 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
           ElevatedButton(
             onPressed: () async {
-              _saveTextFile();
+
+              fileobj.saveTextFile(filename.text, newnote.text);
+              setState(() {
+                x=newnote.text;
+                newnote.text="";
+                filename.text="";
+
+
+              });
             },
             child: const Text('save a text'),
+          ),
+          TextField(
+            controller: dirName,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'file Name',
+              hintText: 'Enter the a file to name to save the note to ',
+            ),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              fileobj.creatDir(dirName.text);
+            },
+            child: const Text('create Dir'),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              fileobj.getthepath();
+            },
+            child: const Text('get the path'),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              List<Map> response= await sqlDb.readData("SELECT count(*) as 'number of students added to the db' FROM std_info2");
+              print(response);
+
+            },
+            child: const Text('slect all db'),
+          ),
+          ElevatedButton(
+            onPressed: () async {
+              int response= await sqlDb.deleteData("DELETE FROM std_info2");
+              print(response);
+            },
+            child: const Text('delete db'),
           ),
 
 
         ]),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _readTextFile,
+        onPressed: (){
+          setState(() {
+            filecontaint=fileobj.readTextFile();
+            print(filecontaint);
+            //fileobj.getthepath();
+          });
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
---
+++ lib/textfiles.dart
@@ -0,0 +1,153 @@
+import 'dart:convert';
+import 'dart:io';
+import 'main.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:path_provider/path_provider.dart';
+import 'sqldb.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+class Textfiles {
+  SqlDb sqlDb = SqlDb();
+
+  Future<String> readTextFile() async{
+    final Directory tempDir = await getTemporaryDirectory();
+    print(tempDir);
+
+    final File file = File('${tempDir.path}/file_picker/hihihi.text');
+
+    final String fileContent = (await file.readAsString()) ;
+
+    print(fileContent.toString());
+
+
+    return(fileContent);
+
+  }
+//this fuction will read a students list from text file (in the formate of comma Delimited CSV) to a table in a database
+  Future<String> readStudentslist(File? filepath) async{
+
+
+    final List<String> fileContent2 = await filepath!.readAsLines();
+
+    print("=====================");
+    int count=0;
+    String department;
+    String? departmentID;
+    String? level;
+    int? intlevel;
+
+
+    for (var i in fileContent2){
+      //print(i);
+      if ((count==1)){
+        List<String> s2=i.split(",");
+        department=s2[4];
+        //    print(department);
+
+        List<Map> response2= await sqlDb.readData("SELECT DeptID FROM department WHERE DeptName = '${department}'");
+        print(response2);
+
+        //  print(departmentID+"hhh");
+        if(response2.isEmpty){
+
+          String sqlQ = "INSERT INTO department ('DeptName') VALUES ('${department}')";
+          int response = await sqlDb.insertData(sqlQ);
+          print("this department is not in the database, now is add "+response.toString());
+          departmentID=response.toString();
+          print(departmentID);
+
+        }else{
+          departmentID=response2[0]['DeptID'].toString();
+
+        }
+        //print(response2[0]['DeptID']);
+//print(departmentID);
+      }
+      if ((count==2)){
+        List<String> s2=i.split(",");
+        level=s2[4];
+        //print(s2[4].toString());
+        if (level=="الاول") intlevel=1;
+        else if (level=="الثاني") intlevel=2;
+        else if (level=="الثالث") intlevel=3;
+        else intlevel=4;
+        //print(intlevel);
+      }
+
+      if ((count > 6)){
+        List<String> s2=i.split(",");
+        if(s2[0]!="") {
+          print("ادخال بيانات الطالب "+s2[1] + "مسجلة برقم قيد " + s2[2]+"  الى قاعدة البيانات  ");
+          String sqlQ = "INSERT INTO std_info2 ('StdID','DeptID','StdName','level') VALUES ('${s2[2]}','${departmentID}','${s2[1]}','${intlevel}')";
+          int response = await sqlDb.insertData(sqlQ);
+          print("");
+        }
+
+      }
+
+      count++;
+    }
+    // String sqlQ="SELECT * FROM std_info2";
+    //List<Map> response = await sqlDb.readData(sqlQ);
+    //print(response);
+    //sqlDb.insertData(sqlQ);
+    //print("LINE001"+fileContent2[0].toString());
+    // print("LINE002"+fileContent2[1].toString());
+    // print("LINE003"+fileContent2[2].toString());
+
+
+    x=fileContent2[2];
+
+    return(fileContent2[2]);
+  }
+
+  saveTextFile(String filename, String containt) async{
+    // this next 4 line to write a file in temp Directory
+    // final Directory tempDir = await getTemporaryDirectory();
+    final File file = File('/storage/emulated/0/Documents/'+filename+".text");
+    // print(""+tempDir.toString());
+    var status = await Permission.storage.status;
+    if (!status.isGranted) {
+      print("no permistiom");
+      await Permission.storage.request();
+    }
+    await file.writeAsString(containt);
+
+  }
+
+  creatDir(String dirname) async {
+
+    ////////////here we creat a file in side our own Dir in temp folder
+    final Directory tempDir = await getTemporaryDirectory();
+    final Directory newDirectory = Directory('/storage/emulated/0/'+dirname);
+
+// Always check that the directory exists
+    if (await newDirectory.exists() == false) {
+      await newDirectory.create();
+    }
+
+    final File file = File('${newDirectory.path}/sample_file.txt');
+    print(""+tempDir.toString());
+    await file.writeAsString('this file writen as the try ');
+  }void
+
+  getthepath() async {
+
+// Put cache files in this directory
+    final temporaryDirectory = await getTemporaryDirectory();
+
+    print("the temporaryDirectory is :");
+    print(temporaryDirectory);
+// For files that our app uses but are not exposed to the user
+    final appSupport = await getApplicationSupportDirectory();
+    print(" the appSupport is :");
+    print(appSupport);
+
+    // For user-generated files
+    final appDocuments = await getApplicationDocumentsDirectory();
+    print("the appDocuments is :");
+    print(appDocuments);
+
+  }
+
+}
